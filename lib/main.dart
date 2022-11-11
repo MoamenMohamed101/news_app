e8cdbf5ae4788d53c574f80abc5f0c0c33dc0a88
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-11-11 15:27:57 +0200

    news_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:news_app/layout/new_layout.dart';
 
 main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: Colors.red,
+              statusBarIconBrightness: Brightness.light),
+        ),
+      ),
       home: NewsLayout(),
     ),
   );
